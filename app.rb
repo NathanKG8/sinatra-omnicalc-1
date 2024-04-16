4a23054bbc73b3bb60c1d745515d8c8e7dc46dcd
NathanKG8 <164825742+NathanKG8@users.noreply.github.com>
2024-04-16 03:16:46 +0000

    Some initial progress

--- app.rb
+++ app.rb
@@ -1,6 +1,18 @@
 require "sinatra"
 require "sinatra/reloader"
 
+get("/howdy") do
+  erb(:hello)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get("/square/results") do
+  erb(:square_results)
+end
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
