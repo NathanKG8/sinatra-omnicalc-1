c384b47cfd55e497cd511bb585d95dc39fb1bf84
NathanKG8 <164825742+NathanKG8@users.noreply.github.com>
2024-04-16 03:36:42 +0000

    progress

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/howdy") do
-  erb(:hello)
+get("/") do
+  erb(:new_square_calc)
 end
 
 get("/square/new") do
@@ -10,12 +10,41 @@ get("/square/new") do
 end
 
 get("/square/results") do
+  @beforefloat_num = params.fetch("number")
+  @the_num = params.fetch("number").to_f
+  @the_result = @the_num ** 2
   erb(:square_results)
 end
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/square_root/new") do
+  erb(:new_square_calc)
+end
+
+get("/square_root/results") do
+  @beforefloat_num = params.fetch("number")
+  @the_num = params.fetch("number").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
+get("/payment/new") do
+  erb(:new_square_calc)
+end
+
+get("/payment/results") do
+  @beforefloat_num = params.fetch("number")
+  @the_num = params.fetch("number").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
+end
+
+get("/random/new") do
+  erb(:new_square_calc)
+end
+
+get("/random/results") do
+  @beforefloat_num = params.fetch("number")
+  @the_num = params.fetch("number").to_f
+  @the_result = @the_num ** 2
+  erb(:square_results)
 end
